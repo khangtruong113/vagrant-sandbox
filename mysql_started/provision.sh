2a4eed3b3e6ed08a27438e8ea2fba1a2f6281f4e
unknown <khang.truongbinh@gmail.com>
2022-06-17 11:29:01 +0700

    first commit

---
+++ mysql_started/provision.sh
@@ -0,0 +1,33 @@
+
+echo "Installing Dependencies..."
+export DEBIAN_FRONTEND="noninteractive";
+sudo apt-get update
+sudo apt-get install -y debconf-utils vim curl
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-8.0'
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+sudo -E dpkg -i mysql-apt-config_0.8.10-1_all.deb
+sudo apt-get update
+
+# Install MySQL 8
+echo "Installing MySQL 8..."
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password root'
+sudo debconf-set-selections <<< 'myvsql-community-server mysql-community-server/root-pass password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo -E apt-get -y install mysql-server
+
+# Override any existing bind-address to be 0.0.0.0 to accept connections from host
+echo "Updating my.cnf..."
+sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+echo "[mysqld]" | sudo tee -a /etc/mysql/my.cnf
+echo "bind-address=0.0.0.0" | sudo tee -a /etc/mysql/my.cnf
+echo "default-time-zone='+00:00'" | sudo tee -a /etc/mysql/my.cnf
+
+echo "Granting root access via any IP..."
+MYSQL_PWD=root mysql -u root -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+
+# Start MySQL server
+echo "Restarting MySQL..."
+sudo service mysql restart
+
+echo "Provisioning Complete"
